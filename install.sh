5c78ee15a4e3a97d66b4a7d47b241885c511e550
Christian Skeide <37899951+cskeide@users.noreply.github.com>
2018-04-29 11:51:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,4 @@ ln -sfv "$DOTFILES_DIR/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/.bash_aliases" ~
 ln -sfv "$DOTFILES_DIR/.nanorc" ~
-ln -sfv "$DOTFILES_DIR/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/.vimrc" ~

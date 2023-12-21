67fb1fe9cc9f2de4d8dc21787504fb358dab4a23
Christian Kironborg Skeide <37899951+cskeide@users.noreply.github.com>
2023-12-21 10:00:44 +0100

    Added fish config file

---
+++ config.fish
@@ -0,0 +1,49 @@
+function fish_greeting
+end
+
+if status is-interactive
+	# Commands to run in interactive sessions can go here
+	if test -d $HOME/bin
+		fish_add_path $HOME/bin
+	end
+
+	set EDITOR /usr/bin/vim
+	set GIT_EDITOR /usr/bin/vim
+	
+	# ls aliases
+	alias la='ls -A'
+	alias ll='ls -lAh'
+
+	# use vim not vi
+	alias vi 'vim'
+
+	# Privileged access
+	if not fish_is_root_user;
+	    alias sudo='sudo '
+	    alias scat='sudo cat'
+	    alias svim='sudoedit'
+	    alias root='sudo -i'
+	    alias reboot='sudo systemctl reboot'
+	    alias poweroff='sudo systemctl poweroff'
+	end
+	
+	# misc
+	alias df='df -H'
+	alias du='du -ch'
+	alias free='free -h'
+	alias diff='colordiff'
+	alias grep='grep --color=auto'
+	alias more='less'
+	alias ip='ip -c'
+
+	# Safety features
+	alias cp='cp -i'
+	alias mv='mv -i'
+	alias rm='rm -I'                    # 'rm -i' prompts for every file
+	alias ln='ln -i'
+	alias chown='chown --preserve-root'
+	alias chmod='chmod --preserve-root'
+	alias chgrp='chgrp --preserve-root'
+end
+
+

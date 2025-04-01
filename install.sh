140531779e3f44aba8e814161a8dbc73c045f90d
Christian Skeide <37899951+cskeide@users.noreply.github.com>
2025-04-01 21:55:49 +0200

    Make sure we are in dotfiles dir when running stow

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cd $DOTFILES_DIR
 rm -vf ~/.bashrc ~/.bash_profile ~/.bash_aliases ~/.config/fish/config.fish ~/.vimrc ~/.nanorc
 stow -v bash fish nano vim

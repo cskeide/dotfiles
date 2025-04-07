a5a8e636a24f0e4f621f11c6d248d9c6fd0224ce
Christian Skeide <37899951+cskeide@users.noreply.github.com>
2025-04-07 10:36:46 +0200

    Added wlogout

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DOTFILES_DIR
-rm -vr ~/.bash_aliases ~/.bash_profile ~/.bashrc ~/.config/fish/config.fish ~/.vimrc ~/.nanorc ~/.tmux.conf ~/.config/hypr ~/.config/waybar ~/.config/wofi ~/.config/kitty
-stow -v bash fish vim nano tmux hypr waybar wofi kitty
+rm -vr ~/.bash_aliases ~/.bash_profile ~/.bashrc ~/.config/fish/config.fish ~/.vimrc ~/.nanorc ~/.tmux.conf ~/.config/hypr ~/.config/waybar ~/.config/wofi ~/.config/kitty ~/.config/wlogout
+stow -v bash fish vim nano tmux hypr waybar wofi kitty wlogout

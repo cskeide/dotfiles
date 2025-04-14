001cd6deac7be6a7f016c6eb05960baeab530b20
Christian Kronborg Skeide <37899951+cskeide@users.noreply.github.com>
2025-04-14 12:44:27 +0200

    Add completions and formatters

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,11 @@
-  return
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "lua", "python", "javascript", "typescript", "json", "html", "css", "bash", "markdown"
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}

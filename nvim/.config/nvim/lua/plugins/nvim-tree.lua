1161219eae901d13a0fd6e784132a9b2154fcfb6
Christian Kronborg Skeide <37899951+cskeide@users.noreply.github.com>
2025-04-15 14:17:10 +0200

    Move recommended options from options.lua to nvim-tree setup

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,9 +1,11 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("nvim-tree").setup()
-		local opts = { noremap = true, silent = true }
-		vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", opts)
-	end,
+  "nvim-tree/nvim-tree.lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    require("nvim-tree").setup()
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", opts)
+  end,
 }

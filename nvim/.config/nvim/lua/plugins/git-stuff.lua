a1c7ea1875a7886e67e53732190675e43c835397
Christian Kronborg Skeide <37899951+cskeide@users.noreply.github.com>
2025-04-21 07:08:21 +0200

    Add gitsigns

---
+++ nvim/.config/nvim/lua/plugins/git-stuff.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "tpope/vim-fugitive",
+	  config = function() end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function() 
+      require('gitsigns').setup()
+    end,
+  }
+}

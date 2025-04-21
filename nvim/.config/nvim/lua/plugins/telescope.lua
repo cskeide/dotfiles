28aa1857cc4cc661782f55d666dc97a799ace03a
Christian Kronborg Skeide <37899951+cskeide@users.noreply.github.com>
2025-04-21 19:22:25 +0200

    Include hidden files in telescope live_grep

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,11 @@ return {
           find_files = {
             hidden = true,
           },
+          live_grep = {
+            additional_args = function(opts)
+                return {"--hidden"}
+            end
+          },
         },
       })
 

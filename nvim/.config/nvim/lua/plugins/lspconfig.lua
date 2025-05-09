5074c0df448b889f390f8916ce6a49c3581549b5
Christian Kronborg Skeide <37899951+cskeide@users.noreply.github.com>
2025-05-09 13:06:01 +0200

    mason error workaround. pin to version 1.0.0

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,38 +1,42 @@
-if true then
-  return {}
-end
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      automatic_installation = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        tsserver = {
-          settings = {
-            javascript = {
-              format = {
-                enable = true,
-              },
-              suggest = {
-                autoImports = true,
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        javascript = { "prettier" },
-      },
-    },
-  },
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 }
+--if true then
+--  return {}
+--end
+--return {
+--  {
+--    "williamboman/mason-lspconfig.nvim",
+--    opts = {
+--      automatic_installation = true,
+--    },
+--  },
+--  {
+--    "neovim/nvim-lspconfig",
+--    opts = {
+--      servers = {
+--        tsserver = {
+--          settings = {
+--            javascript = {
+--              format = {
+--                enable = true,
+--              },
+--              suggest = {
+--                autoImports = true,
+--              },
+--            },
+--          },
+--        },
+--      },
+--    },
+--  },
+--  {
+--    "stevearc/conform.nvim",
+--    opts = {
+--      formatters_by_ft = {
+--        javascript = { "prettier" },
+--      },
+--    },
+--  },
+--}

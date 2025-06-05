4517b952d0292f32511cf297d6fd75cd7e71788d
Christian Kronborg Skeide <37899951+cskeide@users.noreply.github.com>
2025-06-05 20:40:03 +0200

    fix: remove workaround

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,10 +1,10 @@
-return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
---if true then
---  return {}
---end
+if true then
+  return {}
+end
+--return {
+--  { "mason-org/mason.nvim", version = "^1.0.0" },
+--  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+--}
 --return {
 --  {
 --    "williamboman/mason-lspconfig.nvim",

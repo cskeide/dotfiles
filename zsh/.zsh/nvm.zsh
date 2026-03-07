85494d2ad836ee6111195bc5dca92f14af2ec90e
Christian Kronborg Skeide <37899951+cskeide@users.noreply.github.com>
2026-03-07 09:51:56 +0100

    fix: Ensure NVM_DIR is always set and created if missing in lazy_nvm function

--- zsh/.zsh/nvm.zsh
+++ zsh/.zsh/nvm.zsh
@@ -4,9 +4,15 @@ function lazy_nvm {
   unset -f node
   unset -f npx
 
-  if [ -d "${HOME}/.nvm" ]; then
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  # NVM_DIR must be user-writable for storing node versions and aliases
+  export NVM_DIR="$HOME/.nvm"
+  [ -d "$NVM_DIR" ] || mkdir -p "$NVM_DIR"
+
+  if [ -s "$NVM_DIR/nvm.sh" ]; then
+    \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  elif [ -s "/usr/share/nvm/nvm.sh" ]; then
+    \. "/usr/share/nvm/nvm.sh"  # This loads nvm (pacman)
+    [ -s "/usr/share/nvm/bash_completion" ] && \. "/usr/share/nvm/bash_completion"
   fi
 }
 

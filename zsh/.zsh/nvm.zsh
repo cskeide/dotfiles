24f64c0590a62a4dc8f8eabb40c3c514c7b4124e
Christian Kronborg Skeide <37899951+cskeide@users.noreply.github.com>
2026-03-08 07:51:33 +0100

    refactor: Replace lazy loading of NVM with direct usage of FNM for Node version management

--- zsh/.zsh/nvm.zsh
+++ zsh/.zsh/nvm.zsh
@@ -1,23 +1,4 @@
-function lazy_nvm {
-  unset -f nvm
-  unset -f npm
-  unset -f node
-  unset -f npx
-
-  # NVM_DIR must be user-writable for storing node versions and aliases
-  export NVM_DIR="$HOME/.nvm"
-  [ -d "$NVM_DIR" ] || mkdir -p "$NVM_DIR"
-
-  if [ -s "$NVM_DIR/nvm.sh" ]; then
-    \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  elif [ -s "/usr/share/nvm/nvm.sh" ]; then
-    \. "/usr/share/nvm/nvm.sh"  # This loads nvm (pacman)
-    [ -s "/usr/share/nvm/bash_completion" ] && \. "/usr/share/nvm/bash_completion"
-  fi
-}
-
-# aliases
-function nvm { lazy_nvm; nvm "$@"; }
-function npm { lazy_nvm; npm "$@"; }
-function node { lazy_nvm; node "$@"; }
-function npx { lazy_nvm; npx "$@"; }
+# fnm (Fast Node Manager) - no lazy loading needed
+if command -v fnm &> /dev/null; then
+  eval "$(fnm env --use-on-cd)"
+fi
